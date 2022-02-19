d348474fd5adf8fd7eb3c79157973d356e191555
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2022-02-19 20:16:35 +0700

    upload source code

---
+++ .history/assets/main_20220219152321.js
@@ -0,0 +1,60 @@
+
+var myIndex = 0;
+        carousel();
+        
+        function carousel() {
+          var i;
+          var x = document.getElementsByClassName("img_dish");
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+          }
+          myIndex++;
+          if (myIndex > x.length) {myIndex = 1}    
+          x[myIndex-1].style.display = "block";  
+          setTimeout(carousel, 3000); 
+        }
+
+        const buyBtns = document.querySelectorAll('.js-buy-ticket')
+		const modal = document.querySelector('.js-modal')
+		const modalClose = document.querySelector('.js-modal-close')
+		const modalContainer = document.querySelector('.js-modal-container')
+
+		function showBuyTickets() {
+			modal.classList.add('open')
+		}
+
+
+		function hideBuyTickets() {
+			modal.classList.remove('open')
+		}
+
+
+		for (const buyBtn of buyBtns) {
+			buyBtn.addEventListener('click', showBuyTickets)
+		}
+
+
+		modalClose.addEventListener('click', hideBuyTickets)
+		modal.addEventListener('click', hideBuyTickets)
+		modalContainer.addEventListener('click', function (event) {
+			event.stopPropagation()
+		})
+
+function open_menu(){
+	var a = document.getElementById('nav_1')
+	if(a.style.display == 'block'){
+		a.style.display = 'none'
+	}else{
+		a.style.display = 'block'
+	}
+}
+
+function open_menu2(){
+	var b = document.getElementById('')
+}
+
+
+
+
+
+
