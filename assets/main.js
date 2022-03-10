2c4bf236eac893b1496ee544372bb9f205a53e49
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2022-03-10 10:43:09 +0700

    upload source code

--- assets/main.js
+++ assets/main.js
@@ -1,57 +1,47 @@
 var myIndex = 0;
-        carousel();
+    carousel();
         
-        function carousel() {
-          var i;
-          var x = document.getElementsByClassName("img_dish");
-          for (i = 0; i < x.length; i++) {
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("img_dish");
+        for (i = 0; i < x.length; i++) {
             x[i].style.display = "none";  
-          }
-          myIndex++;
-          if (myIndex > x.length) {myIndex = 1}    
-          x[myIndex-1].style.display = "block";  
-          setTimeout(carousel, 3000); 
         }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(carousel, 3000); 
+    }
 
-        const buyBtns = document.querySelectorAll('.js-buy-ticket')
-		const modal = document.querySelector('.js-modal')
-		const modalClose = document.querySelector('.js-modal-close')
-		const modalContainer = document.querySelector('.js-modal-container')
+    const buyBtns = document.querySelectorAll('.js-buy-ticket')
+	const modal = document.querySelector('.js-modal')
+	const modalClose = document.querySelector('.js-modal-close')
+	const modalContainer = document.querySelector('.js-modal-container')
+	function showBuyTickets() {
+		modal.classList.add('open')
+	}
+	function hideBuyTickets() {
+		modal.classList.remove('open')
+	}
+	for (const buyBtn of buyBtns) {
+		buyBtn.addEventListener('click', showBuyTickets)
+	}
+	modalClose.addEventListener('click', hideBuyTickets)
+	modal.addEventListener('click', hideBuyTickets)
+	modalContainer.addEventListener('click', function (event) {
+		event.stopPropagation()
+	})
 
-		function showBuyTickets() {
-			modal.classList.add('open')
-		}
 
-
-		function hideBuyTickets() {
-			modal.classList.remove('open')
-		}
-
-
-		for (const buyBtn of buyBtns) {
-			buyBtn.addEventListener('click', showBuyTickets)
-		}
-
-
-		modalClose.addEventListener('click', hideBuyTickets)
-		modal.addEventListener('click', hideBuyTickets)
-		modalContainer.addEventListener('click', function (event) {
-			event.stopPropagation()
-		})
-
-		
 function open_menu(){
 	var a = document.getElementById('nav_1')
 	var b = document.getElementById('nav_2')
-
 	if(a.style.display == 'block' && b.style.display == 'block'){
 		a.style.display = 'none'
 		b.style.display = 'none'
-
 	}else{
 		a.style.display = 'block'
 		b.style.display = 'block'
-
 	}
 }
 function hidden_menu(){
@@ -61,8 +51,9 @@ function hidden_menu(){
 		a.style.display = 'none'
 		b.style.display = 'none'
 	}
-
 }
+
+
 
 $(document).ready(function(){
     $(".counter__number").counterUp({
